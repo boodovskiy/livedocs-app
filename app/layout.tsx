fd3c740bd51b9a9360432f4d55655420bd65661e
boodovskiy <dzak1987@mail.ru>
2025-05-02 09:08:18 +0300

    Add Liveblocks room

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter as FontSans } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
+import Provider from "./Provider";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -38,7 +39,9 @@ export default function RootLayout({
             "min-h-screen font-sans antialiased", fontSans.variable
           )}
         >
-          {children}
+          <Provider>
+            {children}
+          </Provider>
         </body>
       </html>
     </ClerkProvider>
