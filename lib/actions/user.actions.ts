6045f47c0fe9039237b57e72efb863203fb1aa45
boodovskiy <dzak1987@mail.ru>
2025-05-12 09:26:33 +0300

    Add User Mention Feature

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,6 +1,7 @@
 'use server';
 import { createClerkClient } from '@clerk/nextjs/server';
 import { parseStringify } from '../utils';
+import { liveblocks } from '../liveblocks';
 
 const clerkClient = createClerkClient({
     secretKey: process.env.CLERK_SECRET_KEY as string,
@@ -24,5 +25,24 @@ export const getClerkUsers = async ({ userIds }: { userIds: string[]}) => {
         return parseStringify(sortedUsers);
     } catch (error) {
         console.error('Error happened while getting users:', error);
+    }
+}
+
+export const getDocumentUsers = async ({ roomId, currentUser, text }: { roomId: string, currentUser: string, text: string }) => {
+    try {
+        const room = await liveblocks.getRoom(roomId);
+
+        const users = Object.keys(room.usersAccesses).filter((email) => email !== currentUser);
+
+        if (text) {
+            const lowerCaseText = text.toLowerCase();
+
+            const filteredUsers = users.filter((email: string) => email.toLowerCase().includes(lowerCaseText));
+            return parseStringify(filteredUsers);
+        }
+
+        return parseStringify(users);
+    } catch (error) {
+        console.error('Error happened while fetchinig document users:', error);
     }
 }
