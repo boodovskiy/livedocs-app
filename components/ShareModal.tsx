cedc293590f5fa5d7799e8fc05fc9b62cbc65cab
boodovskiy <dzak1987@mail.ru>
2025-05-14 09:49:34 +0300

    Add Invite Button and Collaborators list into ShareModal

--- components/ShareModal.tsx
+++ components/ShareModal.tsx
@@ -13,6 +13,8 @@ import { Button } from './ui/button';
 import Image from 'next/image';
 import { Label } from './ui/label';
 import { Input } from './ui/input';
+import UserTypeSelector from './UserTypeSelector';
+import Collaborator from './Collaborator';
   
 
 const ShareModal = ({ roomId, collaborators, creatorId, currentUserType } : ShareDocumentDialogProps) => {
@@ -27,7 +29,7 @@ const ShareModal = ({ roomId, collaborators, creatorId, currentUserType } : Shar
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
-        <DialogTrigger>
+        <DialogTrigger asChild>
             <Button className='gradient-blue flex h-9 gap-1 px-4' disabled={currentUserType !== 'editor'}>
                 <Image 
                     src='/assets/icons/share.svg'
@@ -62,7 +64,30 @@ const ShareModal = ({ roomId, collaborators, creatorId, currentUserType } : Shar
                         onChange={(e) => setEmail(e.target.value)}
                         className='share-input'
                     />
+                    <UserTypeSelector 
+                        userType={userType}
+                        setUserType={setUserType}
+                    />
                 </div>
+                <Button type='submit' onClick={shareDocumentHandler} className='gradient-blue flex h-full gap-1 px-5' disabled={loading}>
+                    {loading ? 'Sending...' : 'Invite'}
+                </Button>
+            </div>
+            
+            <div className='my-2 space-y-2'>
+                <ul className="flex flex-col">
+                    {collaborators?.map((collaborator) => (
+                        <Collaborator 
+                            key={collaborator.id}
+                            roomId={roomId}
+                            creatorId={creatorId}
+                            email={collaborator.email}
+                            collaborator={collaborator}
+                            user={user.info}
+                        />
+                    ))}
+                </ul>
+
             </div>
         </DialogContent>
     </Dialog>
